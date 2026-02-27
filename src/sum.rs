81b6d6e11359c7ef3653e34a7413d43c4a7d4283
Santosh <gvsantosh.05@gmail.com>
2026-02-27 11:53:42 +0530

    Rust code for sets and vectors

--- src/sum.rs
+++ src/sum.rs
@@ -1,5 +1,33 @@
-// use std::io;
 
-// fn HelloFunc(){
-//     print!("Hello there");
-// }
+use std::{collections::HashSet, io};
+
+fn main(){
+    let mut set: HashSet<i32> = HashSet::new();
+    let mut nums: Vec<i32> = Vec::new();
+    let mut inp = String::new();
+    io::stdin()
+    .read_line(&mut inp)
+    .expect("Fialed to Read Input Data");
+    let mut x:i32 = match inp.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid input. Using default 0.");
+            0
+        }
+    };
+    while x > 0 {
+        let val:i32 = match inp.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid input. Using default 0.");
+            0
+        }
+    };
+        nums.push(val);
+        x -= 1;
+    }
+    for num in nums {
+        set.insert(num);
+    }
+    println!("{:?}",set);
+}
