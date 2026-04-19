878a8f865084e04e63029c50446f6a37d4378b2b
Santosh <gvsantosh.05@gmail.com>
2026-04-19 12:38:56 +0530

    LeetCode Contest

--- src/first_index.rs
+++ src/first_index.rs
@@ -1,7 +1,37 @@
+use std::io;
+
 
 
 fn  main() {
-    
+    let mut input:String = String::new();
+    io::stdin().read_line(&mut input).expect("Error Taking input");
+    let n:i32 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid input. Using default 0.");
+            0
+        }
+    };
+    let mut nums:Vec<i32> = Vec::new();
+    for i in 0..n {
+        let  x:i32 = match input.trim().parse() {
+           Ok(num) => num,
+            Err(_) => {
+                println!("Invalid input. Using default 0.");
+                0
+            } 
+        };
+        nums.push(x);
+    }
+    let  k:i32 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid input. Using default 0.");
+            0
+        }
+    };
+    let res:i32 = first_index(nums, k);
+    println!("{}",res);
 }
 
 pub fn first_index(nums: Vec<i32>, k: i32) -> i32 {
@@ -25,7 +55,7 @@ pub fn first_index(nums: Vec<i32>, k: i32) -> i32 {
 
 pub fn index_diff(nums: &Vec<i32>, i: usize) -> i32 {
     if i == 0 || i == nums.len() {
-        return i32::MAX; // avoid empty slice panic
+        return i32::MAX; 
     }
 
     let beg = &nums[0..i];
